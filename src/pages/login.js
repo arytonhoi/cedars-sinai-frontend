da54437d7a5a98b82c016201af3346ef73204e7c
Clifford Sia <clifford@sia.nz>
2020-10-15 22:00:32 +0100

    synchronise login page to figma

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 
 // MUI Stuff
+import '../css/login.css';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
@@ -20,9 +21,10 @@ class login extends Component {
   constructor() {
     super();
     this.state = {
-      username: '',
+      username: 'staff',
       password: '',
-      errors: {}
+      errors: {},
+      showPw: false
     };
   }
   componentWillReceiveProps(nextProps) {
@@ -43,6 +45,11 @@ class login extends Component {
       [event.target.name]: event.target.value
     });
   };
+  togglePwField = (event) => {
+    this.setState({
+      showPw: !this.state.showPw
+    });
+  };
   render() {
     const {user, classes, UI: { loading }} = this.props;
 console.log(this.props)
@@ -51,34 +58,48 @@ console.log(this.props)
       <Grid container className={classes.form}>
         <Grid item sm />
         <Grid item sm>
-          <Typography variant="h2" className={classes.pageTitle}>
-            Login
-          </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
-            <TextField
-              id="username"
+            <input
+              id="adminSelect"
+              type="radio"
               name="username"
-              type="text"
-              label="Username"
-              className={classes.textField}
-              helperText={errors.username}
-              error={errors.username ? true : false}
-              value={this.state.username}
+              value="admin"
+              class="noselect"
               onChange={this.handleChange}
-              fullWidth
             />
+            <label for="adminSelect" class="select-admin">ADMIN <svg height="50" width="40" class="dialog-label">
+  <polygon points="20,0 40,50 0,50" style={{fill:"#c4c4c4"}} />
+</svg> </label>
+            <input 
+              id="userSelect"
+              type="radio"
+              name="username"
+              value="user"
+              class="noselect"
+              onChange={this.handleChange}
+            />
+            <label for="userSelect" class="select-user">USER  <svg height="50" width="40" class="dialog-label">
+  <polygon points="20,0 40,50 0,50" style={{fill:"#c4c4c4"}} />
+</svg> </label>
+            <span value={ errors }></span>
+            <div class="login-wrapper">
             <TextField
               id="password"
               name="password"
-              type="password"
-              label="Password"
+              type={this.state.showPw ? "text" : "password"}
+              placeholder="Password"
+              style={{background: "#c4c4c4", padding: "0.5em 0", "border-radius":"0.5em"}}
               className={classes.textField}
               helperText={errors.password}
               error={errors.password ? true : false}
               value={this.state.password}
               onChange={this.handleChange}
               fullWidth
+              disableUnderline
             />
+            <span class="key-icon noselect">Key</span>
+            <span class="pw-toggle noselect" onClick={this.togglePwField}>Eye</span>
+            </div>
             {errors.general && (
               <Typography variant="body2" className={classes.customError}>
                 {errors.general}
@@ -88,10 +109,12 @@ console.log(this.props)
               type="submit"
               variant="contained"
               color="primary"
+              style={{background:"linear-gradient(90deg, #999, #434343)"}}
               className={classes.button}
+              fullWidth
               disabled={loading}
             >
-              Login
+              Sign In
               {loading && (
                 <CircularProgress size={30} className={classes.progress} />
               )}
@@ -113,6 +136,7 @@ login.propTypes = {
 
 const mapStateToProps = (state) => ({
   user: state.user,
+  showPw: state.showPw,
   UI: state.UI
 });
 
