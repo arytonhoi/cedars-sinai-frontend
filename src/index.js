728905b517a584cbaad386518a300b51f7e57af7
Clifford Sia <clifford@sia.nz>
2020-11-08 20:22:41 +0000

    change project font to Roboto as per Celine

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 //Back compatibility for ES5
 
+
+//Fonts
+import './roboto.css'
+import './roboto/robotoi1.woff2'; 
+import './roboto/robotoi3.woff2'; 
+import './roboto/robotoi4.woff2'; 
+import './roboto/robotoi5.woff2'; 
+import './roboto/robotoi7.woff2'; 
+import './roboto/roboto1.woff2'; 
+import './roboto/roboto3.woff2'; 
+import './roboto/roboto4.woff2'; 
+import './roboto/roboto5.woff2'; 
+import './roboto/roboto7.woff2'; 
+import './roboto/roboto9.woff2'; 
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
