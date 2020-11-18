f2cb144006e67d6aeb372fb5c9d3b4345c7416b3
Clifford Sia <clifford@sia.nz>
2020-11-18 16:31:01 +0000

    fix breaking changes by using toolbar instead of toolbarGroups in PostAnn.js. No idea why this different syntax broke only in prod

--- src/components/announcement/PostAnn.js
+++ src/components/announcement/PostAnn.js
@@ -55,21 +55,20 @@ class PostAnn extends Component{
             //   name:"Basic",
             //   items:["Bold","Italic","Underline","Superscript","Subscript","Link", "Image"]
             // }]
-            toolbarGroups: [
-              { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-              { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+            toolbar: [
+              { name: 'clipboard',groups: [ 'clipboard', 'undo' ] },
+              { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
               { name: 'links' },
               { name: 'insert' },
               { name: 'forms' },
               { name: 'tools' },
-              { name: 'document',       groups: [ 'mode', 'document', 'doctools' ] },
+              { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
               { name: 'others' },
               '/',
               { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-              { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+              { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
               { name: 'styles' },
               { name: 'colors' },
-              { name: 'about' }
             ]
           }}
         />
