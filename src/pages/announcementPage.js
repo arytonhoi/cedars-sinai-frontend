d220fec815970828ae8fd243fb796c820a9ba0c2
Aryton Hoi <aryton.hoi@bitsighttech.com>
2020-11-30 21:59:30 -0500

    added announcement empty states

--- src/pages/announcementPage.js
+++ src/pages/announcementPage.js
@@ -15,6 +15,7 @@ import {
 
 // components
 import AnnouncementPostEditorModal from "../components/announcement/announcementPostEditorModal.js";
+import AnnouncementList from "../components/announcement/announcementList.js";
 // import BannerImgEditorModal from "../components/announcement/bannerImgEditorModal.js";
 
 // css styles
@@ -23,8 +24,8 @@ import "../css/ckeditor.css";
 import "../components/announcement/announcement.css";
 
 // Ant design
-import { Button, Dropdown, Input, Layout, List, Menu } from "antd";
-import { DownOutlined, EditOutlined, SearchOutlined } from "@ant-design/icons";
+import { Button, Dropdown, Input, Layout, Menu } from "antd";
+import { DownOutlined, SearchOutlined } from "@ant-design/icons";
 const { Content, Footer } = Layout;
 
 class AnnouncementPage extends Component {
@@ -145,7 +146,6 @@ class AnnouncementPage extends Component {
     const { credentials } = this.props.user;
     const isAdmin = credentials.isAdmin;
     const { bannerImgs, filteredAnnouncements } = this.props.data;
-    const maxAnnouncementPreviewHeight = 300;
 
     return (
       <div className="page-container">
@@ -232,73 +232,10 @@ class AnnouncementPage extends Component {
                 <h1>Recent Announcements</h1>
               </div>
             </div>
-
-            <List
-              className="content-card-list announcement-list"
-              itemLayout="vertical"
-              size="large"
-              pagination={{
-                onChange: (page) => {
-                  console.log(page);
-                },
-                pageSize: 6,
-              }}
-              dataSource={filteredAnnouncements}
-              renderItem={(announcement) => (
-                <List.Item key={announcement.id} className="announcement-item">
-                  <header className="announcement-header">
-                    <span className="announcement-title">
-                      <h1>{announcement.title}</h1>
-                      {isAdmin && (
-                        <Button
-                          icon={<EditOutlined />}
-                          onClick={() =>
-                            this.handleEditThisAnnouncement(announcement)
-                          }
-                          type="text"
-                        />
-                      )}
-                    </span>
-                    <h3>{announcement.author}</h3>
-                    <h3>{announcement.createdAt.toString("MM/dd/yy")}</h3>
-                  </header>
-                  <div className="announcement-content-container">
-                    <input
-                      type="checkbox"
-                      className="announcement-expand-toggle-checkbox"
-                      id={announcement.id + "-expand-checkbox"}
-                    />
-                    <div
-                      ref={(element) => {
-                        if (
-                          element &&
-                          element.clientHeight > maxAnnouncementPreviewHeight
-                        ) {
-                          element.className = element.className + " overflowed";
-                        }
-                      }}
-                      className="announcement-content ckeditor-content"
-                      dangerouslySetInnerHTML={{ __html: announcement.content }}
-                    />
-                    <span className="announcement-expand-toggle">
-                      <label
-                        htmlFor={announcement.id + "-expand-checkbox"}
-                        className="ant-btn"
-                      >
-                        Show more
-                      </label>
-                    </span>
-                    <span className="announcement-collapse-toggle">
-                      <label
-                        htmlFor={announcement.id + "-expand-checkbox"}
-                        className="ant-btn"
-                      >
-                        Show less
-                      </label>
-                    </span>
-                  </div>
-                </List.Item>
-              )}
+            <AnnouncementList
+              announcements={filteredAnnouncements}
+              filter={this.state.filter}
+              handleEditThisAnnouncement={this.handleEditThisAnnouncement}
             />
           </Content>
           <Footer style={{ textAlign: "center" }}>DevelopForGood ©2020</Footer>
