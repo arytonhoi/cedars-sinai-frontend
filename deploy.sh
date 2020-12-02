79f1e7ec78b3b51a1d1f671a9427ae0440a2147a
Aryton Hoi <aryton.hoi@bitsighttech.com>
2020-12-01 22:10:07 -0500

    changed deploy.sh to prd and dev

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-if [[ ${1:0:4} == "prod" ]]
+if [[ ${1:0:4} == "prd" ]]
   then
     npm run build
     rm -r ./production
     mv ./build ./production
     firebase-deploy --only hosting:cedars-prd,functions:cedars-prd
-elif [[ ${1:0:4} == "test" ]]
+elif [[ ${1:0:4} == "dev" ]]
   then
     npm run build
     firebase-deploy --only hosting:cedars-dev,functions:cedars-dev
 else
-  echo "Usage: deploy.sh [TARGET] where target is either 'prod' or 'test'."
+  echo "Usage: deploy.sh [TARGET] where target is either 'prd' or 'dev'."
 fi
