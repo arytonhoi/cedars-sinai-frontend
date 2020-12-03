a02895cec02515e4f8138eff4f05f7f5761575de
Aryton Hoi <aryton.hoi@bitsighttech.com>
2020-12-02 21:20:23 -0500

    merge conflicts

--- src/components/folders/Folder.js
+++ src/components/folders/Folder.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Folder.css";
 import PropTypes from "prop-types";
 
-import { FolderFilled } from "@ant-design/icons"
+import { FolderFilled } from "@ant-design/icons";
 
 // Redux stuff
 import { connect } from "react-redux";
@@ -10,10 +10,11 @@ import { connect } from "react-redux";
 
 class Folder extends Component {
   render() {
-    const { label, href } = this.props
-    if(typeof(href)==="function"){
-      return(
-        <div className="folder clickable folder-normal noselect"
+    const { label, href } = this.props;
+    if (typeof href === "function") {
+      return (
+        <div
+          className="folder clickable folder-normal noselect"
           onClick={href}
           onMouseDown={this.props.onMouseDown}
           onMouseUp={this.props.onMouseUp}
@@ -26,9 +27,12 @@ class Folder extends Component {
           </div>
         </div>
       );
-    }else{
-      return(
-        <a className="folder folder-normal noselect" href={"/resources/" + href}>
+    } else {
+      return (
+        <a
+          className="folder folder-normal noselect"
+          href={"/resources/" + href}
+        >
           <div className="folder-link">
             <span className="folder-logo-icon">
               <FolderFilled />
@@ -43,11 +47,13 @@ class Folder extends Component {
 
 Folder.propTypes = {
   label: PropTypes.string.isRequired,
-  href: PropTypes.oneOfType([PropTypes.string,PropTypes.number,PropTypes.func]).isRequired,
+  href: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.func,
+  ]).isRequired,
 };
 
 const mapStateToProps = (state) => ({});
 
-export default connect(mapStateToProps, { })(
-  Folder
-);
+export default connect(mapStateToProps, {})(Folder);
