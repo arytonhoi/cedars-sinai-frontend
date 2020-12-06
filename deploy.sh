db70449c438e7752143482ee760daa6ae9653294
Clifford Sia <clifford@sia.nz>
2020-12-06 21:46:13 +0000

    add dev version of functions

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,13 @@ if [[ $1 == "prd" ]]
     react-scripts build
     rm -r ./production
     mv ./build ./production
-    firebase deploy --only hosting:cedars-prd,functions:cedars-prd
+    firebase deploy --only hosting:cedars-prd,functions:app,functions:onFolderDelete,functions:onDepartmentDelete
 elif [[ $1 == "dev" ]]
   then
+    sed 's/^exports.app/exports.devApp/' -i ./functions/index.js
     npm run build
-    firebase deploy --only hosting:cedars-dev,functions
+    firebase deploy --only hosting:cedars-dev,functions:devApp,functions:onFolderDelete,functions:onDepartmentDelete
+    sed 's/devApp/app/' -i ./functions/index.js
 else
   echo "Usage: deploy.sh [TARGET] where target is either 'prd' or 'dev'."
 fi
